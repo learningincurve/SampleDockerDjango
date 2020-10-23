5c53171abcf2fb31d789ffb93ff01e02fee6dc15
learningincurve <learningincurve@gmail.com>
2020-10-24 00:11:50 +0530

    secure.

--- DockerDjango/DockerDjango/settings.py
+++ DockerDjango/DockerDjango/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+from environ import Env
+env = Env()
+env.read_env(env_file='.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#nwh+sn=i6k697d9vu*iqigqk@k5%2h08n+0^bfm=e)m4%uoxz'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ['*']
 
